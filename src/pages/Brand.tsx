9979022aadb0cb4a8b621b9976c5facd9b2752f6
SoonHyun <suni@team-mono.com>
2024-10-18 06:31:46 +0900

    feat : proxy 추가, board 이미지 추가 기능 진행중

--- src/pages/Brand.tsx
+++ src/pages/Brand.tsx
@@ -2,6 +2,7 @@
 import { PageContainer } from "../components/layouts/PageLayouts";
 import { ReadyBanner } from "../components/Empty/ReadyBanner";
 import { css } from "@emotion/react";
+import { BoardEditor } from "../components/Board/BoardEditor";
 
 export function Brand() {
   return (
@@ -14,6 +15,8 @@ export function Brand() {
         title="브랜드"
         description="AS 및 구매 문의는 전화 (02-2235-3573) 로 부탁드립니다."
       />
+
+      <BoardEditor></BoardEditor>
     </PageContainer>
   );
 }
