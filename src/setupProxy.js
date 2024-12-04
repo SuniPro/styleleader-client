70d3af8f3625d36ec7ce074ab126dc6877c7ada1
Suni <thtnsgus21@gmail.com>
2024-12-05 03:43:42 +0900

    refactor : api 주소 변경

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
   app.use(
     "/api", //proxy가 필요한 path parameter
     createProxyMiddleware({
-      target: "http://localhost:8080", //타겟이 되는 api url
+      target: "http://54.180.230.22:8080", //타겟이 되는 api url
       changeOrigin: true, // 서버 구성에 따른 호스트 헤더 변경 여부 설정
     }),
   );
